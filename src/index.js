9938d3f9a8f565ff9f3034307c777cff0d41dab9
Jeffrey Clarke <jeffreyclarke@jeffreys-mbp.hamilton.edu>
2019-09-27 16:18:45 -0400

    Learning React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const elem = ""
+const tasks = ['mow the lawn', 'take out the trash', 'shovel the snow'];
+const element = React.createElement('ol', null, 
+    tasks.map((task, index) => React.createElement('li', { key: index}, task))
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(element, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
